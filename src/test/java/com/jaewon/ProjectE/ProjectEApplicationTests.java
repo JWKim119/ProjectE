bd277a5c9f9bf3bbfd346efb652484666fc6c7ad
JWKim119 <jaewonk7@gmail.com>
2024-04-04 11:38:55 +0900

    파일 업로드

---
+++ src/test/java/com/jaewon/ProjectE/ProjectEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.ProjectE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
